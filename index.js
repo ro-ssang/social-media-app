2f706635b4a32b673588c995d2c14bb0742ca0df
ro-ssang <fashionfish@naver.com>
2021-11-30 18:24:20 +0900

    feat. MongoDB Cluster 연결

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log('Connected to MongoDB');
+  }
+);
 
 const app = express();
 
 app.listen(8800, () => {
-  console.log('Backend server is running');
+  console.log('Backend server is running!');
 });
