f4c18f8d72413f212c0a96f21aa33c5198cdacbe
ro-ssang <fashionfish@naver.com>
2021-11-30 18:49:09 +0900

    feat. routes 생성
    * auth: 인증 관련
    * user: 유저 관련

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+
 dotenv.config();
 
 mongoose.connect(
@@ -24,6 +27,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
 
+// routes
+app.use('/api/auth', authRoute);
+app.use('/api/user', userRoute);
+
+// running server
 app.listen(8800, () => {
   console.log('Backend server is running!');
 });
