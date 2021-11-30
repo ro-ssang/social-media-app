3b8c0806e5fd438581c3e1c2ca09dc8a9468848f
ro-ssang <fashionfish@naver.com>
2021-11-30 18:28:27 +0900

    feat. 미들웨어 설정
    * express.json(): body parser
    * helmet: 보안 관련
    * morgan: logger

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 dotenv.config();
 
@@ -16,6 +18,11 @@ mongoose.connect(
 );
 
 const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
 
 app.listen(8800, () => {
   console.log('Backend server is running!');
